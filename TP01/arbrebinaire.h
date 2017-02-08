fdc497d4c1eaa3afc954d9f4d15bebfec2b244f1
François Brisson <francois.brisson0@gmail.com>
2017-02-08 13:21:23 -0500

    Ajouter/contains/min/max

--- TP01/arbrebinaire.h
+++ TP01/arbrebinaire.h
@@ -1,5 +1,6 @@
 #ifndef ARBREBINAIRE_H__
 #define ARBREBINAIRE_H__
+#include <float.h>
 
 
 template <class T>
@@ -30,7 +31,7 @@ private:
 
 		Noeud(const T& valeur, Noeud* gauche, Noeud* droite) : valeur(valeur), gauche(gauche), droite(droite) {}
 	};
-	Noeud* m_debut = 0;
+	Noeud* m_debut = nullptr;
 	int m_count = 0;
 };
 
@@ -54,34 +55,43 @@ template <class T>
 void ArbreBinaire<T>::Ajouter(const T& valeur)
 {
 	Noeud* n = m_debut;
-	Noeud* precedent = nullptr;
 
 	if (m_count == 0)
+	{
 		m_debut = new Noeud*(valeur);
-	/*if (n->valeur == valeur)
-		return;*/
+		++m_count;
+	}
 
-	do
+	while (n->valeur != valeur)
 	{
 		if (valeur < n->valeur)
 		{
-			precedent = n;
-			n = n->gauche;
-			if (n->valeur < n->droite->valeur && !n->gauche)
-				n->gauche == new Noeud*(valeur);
+			if (!n->gauche)
+			{
+				n->gauche = new Noeud*(valeur);
+				++m_count;
+				n = n->gauche;
+			}
+			else
+			{
+				n = n->gauche;
+			}
 		}
+
 		else if (valeur > n->valeur)
 		{
-			precedent = n;
-			n = n->droite;
+			if (!n->droite)
+			{
+				n->droite = new Noeud*(valeur);
+				++m_count;
+				n = n->droite;
+			}
+			else
+			{
+				n = n->droite;
+			}
 		}
-		else
-		{
-			
-			++m_count;
-		}
-	} 
-	while (n->valeur != valeur);
+	}
 
 }
 
@@ -93,11 +103,37 @@ void ArbreBinaire<T>::Enlever(const T& valeur)
 template <class T>
 void ArbreBinaire<T>::Vider()
 {
+
 }
 
 template <class T>
 bool ArbreBinaire<T>::Contient(const T& valeur)
 {
+	Noeud* n = m_debut;
+
+	if (m_count == 0)
+		return false;
+
+	while (n->valeur != valeur)
+	{
+		if (valeur < n->valeur)
+		{
+			if (!n->gauche)
+				return false;
+			else
+				n = n->gauche;
+		}
+
+		else if (valeur > n->valeur)
+		{
+			if (!n->droite)
+				return false;
+			else
+				n = n->droite;
+		}
+	}
+
+	return true;
 }
 
 template <class T>
@@ -109,11 +145,39 @@ int ArbreBinaire<T>::Nombre() const
 template <class T>
 const T& ArbreBinaire<T>::Minimum() const
 {
+	Noeud* n = m_debut;
+
+	if (m_count == 0)
+		return;
+
+	while (n)
+	{
+		if (n->gauche)
+			n = n->gauche;
+		else
+			return n;
+	}
+
+	return;
 }
 
 template <class T>
 const T& ArbreBinaire<T>::Maximum() const
 {
+	Noeud* n = m_debut;
+
+	if (m_count == 0)
+		return;
+
+	while (n)
+	{
+		if (n->droite)
+			n = n->droite;
+		else
+			return n;
+	}
+
+	return;
 }
 
 template <class T>
